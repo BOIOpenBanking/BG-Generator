714f361d3e3229a2f2adcbf9502acba483564b2d
BOIOpenBanking <51399040+BOIOpenBanking@users.noreply.github.com>
2019-07-02 11:40:37 +0300

    Update getAccountList.js

--- snipets/getAccountList.js
+++ snipets/getAccountList.js
@@ -1,23 +1,32 @@
 var userId = "";
 if (apim.getvariable('request.authorization')) {
-    userId = apim.getvariable('oauth.resource-owner');
+  userId = apim.getvariable('oauth.resource-owner');
 }
 
 apim.readInputAsJSON(function(err, bankData) {
-    var result = {};
-    try {
-        var myAccountsInfo = bankData.allAccounts.find(item => item.id === userId);
-        result = myAccountsInfo.accountsData;
+  var result = {};
+  try {
+    var myAccountsInfo = bankData.allAccounts.find(item => item.id === userId);
 
-        if (!result) {
-            result = {};
-        }
-    } catch (e) {
-        console.error(e);
-        
-        result = {};
+    var withBalanceQuery = apim.getvariable('request.parameters.withBalance') || true;
+    if (withBalanceQuery === "false") {
+      //myAccountsInfo.accountsData.accounts.map(function(item) {
+      //  delete item.bad;
+      //  return item;
+      //});
+      myAccountsInfo.accountsData.accounts.forEach(function(item, index) {
+        delete item._links;
+      });
     }
+    result = myAccountsInfo.accountsData;
 
-    apim.output('application/json');
-    session.output.write(result);
+    if (!result) {
+      result = {};
+    }
+  } catch (e) {
+    result = e;
+  }
+
+  apim.output('application/json');
+  session.output.write(JSON.stringify(result, null, 2));
 });
