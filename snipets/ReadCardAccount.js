4b8b01e1db472de2872d86ab224ab7f12bb4787d
BOIOpenBanking <51399040+BOIOpenBanking@users.noreply.github.com>
2019-07-02 14:45:13 +0300

    Create ReadCardAccount.js

---
+++ snipets/ReadCardAccount.js
@@ -0,0 +1,25 @@
+var accountId = apim.getvariable('request.parameters.account-id');
+
+var userId = "";
+if (apim.getvariable('request.authorization')) {
+  userId = apim.getvariable('oauth.resource-owner');
+}
+
+apim.readInputAsJSON(function(err, bankData) {
+  var result = {};
+  try {
+    var myAccountsInfo = bankData.allAccounts.find(item => item.id === userId);
+    var myCardAccounts = myAccountsInfo.cardAccountsData;
+    var reqCardAccount = myCardAccounts.cardAccounts.find(item => item.resourceId === accountId);
+    result.cardAccount = reqCardAccount;
+
+    if (!result) {
+      result = {};
+    }
+  } catch (e) {
+    result = e;
+  }
+
+  apim.output('application/json');
+  session.output.write(JSON.stringify(result, null, 2));
+});
