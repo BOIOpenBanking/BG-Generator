cd42c249e82d21fed299d8cce5dcdf7f7310e22d
BOIOpenBanking <51399040+BOIOpenBanking@users.noreply.github.com>
2019-07-03 23:08:38 +0300

    Create getCardAccountBalances.js

---
+++ snipets/getCardAccountBalances.js
@@ -0,0 +1,26 @@
+var accountId = apim.getvariable('request.parameters.account-id');
+
+var userId = "";
+if (apim.getvariable('request.authorization')) {
+    userId = apim.getvariable('oauth.resource-owner');
+}
+
+apim.readInputAsJSON(function(err, bankData) {
+    var result = {};
+    try {
+        var myAccountsInfo = bankData.allAccounts.find(item => item.id === userId);
+        var myCardAccounts = myAccountsInfo.cardAccountsData;
+        var reqCardAccount = myCardAccounts.cardAccounts.find(item => item.resourceId === accountId);
+        var reqAccountMaskedPan = reqCardAccount.maskedPan;
+        result = myAccountsInfo.cardAccountsBalances.find(item => item.cardAccount.maskedPan === reqAccountMaskedPan);
+
+        if (!result) {
+            result = {};
+        }
+    } catch (e) {
+        result = e;
+    }
+
+    apim.output('application/json');
+    session.output.write(JSON.stringify(result, null, 2));
+});
