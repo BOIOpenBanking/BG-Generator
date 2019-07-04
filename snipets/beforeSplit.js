934bddfa063ab61ec9bad8378112ee9e9c48b2b2
BOIOpenBanking <51399040+BOIOpenBanking@users.noreply.github.com>
2019-07-04 14:12:53 +0300

    Update beforeSplit.js

--- snipets/beforeSplit.js
+++ snipets/beforeSplit.js
@@ -1,24 +1,30 @@
 var userId =  apim.getvariable('oauth.resource-owner') || null;
 
 if (userId ) {
-    var requestId = apim.getvariable('request.headers.x-request-id');
+  var requestId = apim.getvariable('request.headers.x-request-id');
 
-    if (!requestId ) {
-        apim.error('Error', 500, 'Validation Error', 'Missing X-Request-ID');
-    } else {
-        var operationId = apim.getvariable('api.operation.id');
-        if (operationId != 'createConsent') {
-            var oauthConsentId = apim.getvariable('oauth.miscinfo').split(':')[1];
-            var consentId = apim.getvariable('request.headers.consent-id');
+  if (!requestId) {
+    apim.setvariable('error_code', 'FORMAT_ERROR', 'set');
+    apim.setvariable('http_code', '500', 'set');
+    apim.error('BG_ERROR');            }
+  } else {
+    var operationId = apim.getvariable('api.operation.id');
+    if (operationId != 'createConsent') {
+      var oauthConsentId = apim.getvariable('oauth.miscinfo').split(':')[1];
+      var consentId = apim.getvariable('request.headers.consent-id');
 
-            if (!consentId ) {
-                apim.error('Error', 500, 'Validation Error', 'Missing Consent-ID');
-            } else if (oauthConsentId != consentId) {
-                apim.error('Error', 500, 'Runtime Error', 'Mismatch Consent-ID');
-            }
-        }
+      if (!consentId) {
+        apim.setvariable('error_code', 'CONSENT_UNKNOWN', 'set');
+        apim.setvariable('http_code', '500', 'set');
+        apim.error('BG_ERROR');            }
+      } else if (oauthConsentId != consentId) {
+        apim.setvariable('error_code', 'CONSENT_UNKNOWN', 'set');
+        apim.setvariable('http_code', '500', 'set');
+        apim.error('BG_ERROR');
+      }
     }
+  }
 }
 else {
-    apim.setvariable('oauth.resource-owner', "", "set")
+  apim.setvariable('oauth.resource-owner', "", "set")
 }
