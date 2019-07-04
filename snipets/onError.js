9ed3011eac4881dd0c556d5e6244d3ff47cd0c32
BOIOpenBanking <51399040+BOIOpenBanking@users.noreply.github.com>
2019-07-04 14:16:07 +0300

    Update onError.js

--- snipets/onError.js
+++ snipets/onError.js
@@ -2,18 +2,18 @@ var error_code = apim.getvariable("error_code");
 var http_code = apim.getvariable("http_code") || "500";
 
 apim.readInputAsJSON(function(err, bankData) {
-    var result = {};
+  var result = {};
 
-    if (err) {
-        result = err;
-        http_code = "999";
-    } else {
-        result = bankData.allErrors.find(item => item.code === error_code) || {"title": "Internal Error"};
-    }
+  if (err) {
+      result = err;
+      http_code = "999";
+  } else {
+      result = bankData.allErrors.find(item => item.code === error_code) || {"title": "Internal Error"};
+  }
 
-    apim.setvariable('message.status.code', http_code, "set");
-    //apim.setvariable('message.body', result, "set");
-    //apim.setvariable('message.headers.content-type', "application/problem+json", "set");
-    apim.output('application/problem+json');
-    session.output.write(JSON.stringify(result, null, 2));
+  apim.setvariable('message.status.code', http_code, "set");
+  //apim.setvariable('message.body', result, "set");
+  //apim.setvariable('message.headers.content-type', "application/problem+json", "set");
+  apim.output('application/problem+json');
+  session.output.write(JSON.stringify(result, null, 2));
 });
